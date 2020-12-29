41211a15e872485b81f356646e221afe5637368b
Athlon1600 <Athlon2400@gmail.com>
2020-12-29 17:48:42 -0600

    download different nginx conf for ubuntu20

--- nginx-ubuntu20.sh
+++ nginx-ubuntu20.sh
@@ -21,7 +21,7 @@ sudo apt-get -y install composer
 composer create-project athlon1600/php-proxy-app:dev-master /var/www/proxy --no-interaction
 
 ## Download configuration
-wget https://raw.githubusercontent.com/Athlon1600/php-proxy-installer/master/nginx/proxy.conf -O /etc/nginx/sites-available/default
+wget https://raw.githubusercontent.com/Athlon1600/php-proxy-installer/master/nginx/ubuntu20.conf -O /etc/nginx/sites-available/default
 
 ## Enable status page for PHP-FPM at: /fpm_status
 sed -i -e "s/;pm.status_path = \/status/pm.status_path = \/fpm_status/g" /etc/php/7.4/fpm/pool.d/www.conf
